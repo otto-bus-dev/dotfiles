0ef3efdd20242dd15182c797acf1602179d34a28
otto <otto.bus.dev@gmail.com>
2025-05-09 15:31:58 +0200

    handle nvidia or intel graphics on install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ~/dotfiles/install/yay.sh
 ~/dotfiles/install/packages.sh
+~/dotfiles/install/graphics.sh
 ~/dotfiles/install/services.sh
 ~/dotfiles/install/config.sh
 ~/dotfiles/install/blender.sh
---
+++ install/graphics.sh
@@ -0,0 +1,48 @@
+nc#!/bin/bash
+
+# Function to detect the GPU
+detect_gpu() {
+    echo "Detecting GPU..."
+    if lspci | grep -i "NVIDIA" > /dev/null; then
+        echo "Detected NVIDIA GPU"
+        GPU="NVIDIA"
+    elif lspci | grep -i "Intel" > /dev/null; then
+        echo "Detected Intel GPU"
+        GPU="Intel"
+    else
+        echo "No compatible GPU detected or GPU not supported"
+        GPU="None"
+    fi
+}
+
+# Function to install NVIDIA drivers
+install_nvidia_driver() {
+    echo "Installing NVIDIA drivers..."
+    yay -S --noconfirm nvidia-open nvidia-utils	nvidia-settings nvidia-dkms
+}
+
+# Function to install Intel drivers
+install_intel_driver() {
+    echo "Installing Intel drivers..."
+    yay -S --noconfirm xf86-video-intel
+}
+
+# Main script logic
+main() {
+    detect_gpu
+
+    case $GPU in
+        "NVIDIA")
+            install_nvidia_driver
+            ;;
+        "Intel")
+            install_intel_driver
+            ;;
+        "None")
+            echo "No drivers installed. Exiting."
+            ;;
+    esac
+}
+
+# Execute the main function
+main
