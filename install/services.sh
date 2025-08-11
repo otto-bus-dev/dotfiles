098468793a1a4c7f24c1325b9b3d906bb7ff5e1e
otto-bus-dev <otto.bus.dev@gmail.com>
2025-08-11 12:38:30 +0200

    reorder install order

--- install/services.sh
+++ install/services.sh
@@ -5,6 +5,6 @@ systemctl --user enable --now hyprpaper.service
 sudo systemctl enable NetworkManager 
 sudo systemctl start NetworkManager 
 sudo systemctl enable greetd
-sudo systemctl start greetd
 sudo systemctl enable --now cups.service
 sudo systemctl start cups.service
+sudo systemctl start greetd
