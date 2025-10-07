a7df5f20cdb9da2a245de029a3a06059410ed354
otto-bus-dev <otto.bus.dev@gmail.com>
2025-10-07 10:34:42 +0200

    change linter icons

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,186 +1,200 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  -- LSP Config
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp", -- autocompletion source for lsp
-    },
-    config = function()
-      -- local lspconfig = require("lspconfig")
-      local cmp_nvim_lsp = require("cmp_nvim_lsp")
-      local null_ls = require("null-ls")
-      local on_attach = function(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        -- LSP keymaps
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        -- Autoformat on save
-        if client.server_capabilities.documentFormattingProvider then
-          vim.cmd([[
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- LSP Config
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- autocompletion source for lsp
+		},
+		config = function()
+			-- local lspconfig = require("lspconfig")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
+			local null_ls = require("null-ls")
+			local on_attach = function(client, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				-- LSP keymaps
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				-- Autoformat on save
+				if client.server_capabilities.documentFormattingProvider then
+					vim.cmd([[
             					augroup LspAutocommands
               						autocmd! * <buffer>
               						autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ timeout_ms = 1000 })
             					augroup END
           				]])
-        end
-      end
+				end
+			end
 
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●", -- or "E", or ""
-          source = "always",
-          spacing = 2,
-        },
-        signs = true,
-        underline = true,
-        update_in_insert = true,
-        severity_sort = true,
-      })
+			local signs = { Error = "", Warn = "", Hint = "⚑", Info = "" }
+			for type, icon in pairs(signs) do
+				vim.fn.sign_define("DiagnosticSign" .. type, { text = icon, texthl = "DiagnosticSign" .. type })
+			end
 
-      -- Retrieve the environment variable
-      local python_path = os.getenv("BLENDER_PYTHON_PATH")
-      local blender_lib_path = os.getenv("BLENDER_PYTHON_LIB_PATH")
-      local blender_bl_operators_path = os.getenv("BLENDER_PYTHON_BL_OPERATORS")
-      -- notify the user if the environment variable is not set
-      if not python_path or python_path == "" then
-        vim.notify(
-          "BLENDER_PYTHON_PATH environment variable shoud be set to enable PYTHON debugging for blender",
-          vim.log.levels.ERROR
-        )
-      end
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = function(diagnostic)
+						-- Map LSP severity to your custom icons
+						local icons = {
+							[vim.diagnostic.severity.ERROR] = signs.Error,
+							[vim.diagnostic.severity.WARN] = signs.Warn,
+							[vim.diagnostic.severity.HINT] = signs.Hint,
+							[vim.diagnostic.severity.INFO] = signs.Info,
+						}
+						return icons[diagnostic.severity] or "●"
+					end,
+					-- prefix = "●", -- or "E", or ""
+					source = "always",
+					spacing = 2,
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = true,
+				severity_sort = true,
+			})
 
-      -- Capabilities for autocompletion
-      local capabilities = cmp_nvim_lsp.default_capabilities()
+			-- Retrieve the environment variable
+			local python_path = os.getenv("BLENDER_PYTHON_PATH")
+			local blender_lib_path = os.getenv("BLENDER_PYTHON_LIB_PATH")
+			local blender_bl_operators_path = os.getenv("BLENDER_PYTHON_BL_OPERATORS")
+			-- notify the user if the environment variable is not set
+			if not python_path or python_path == "" then
+				vim.notify(
+					"BLENDER_PYTHON_PATH environment variable shoud be set to enable PYTHON debugging for blender",
+					vim.log.levels.ERROR
+				)
+			end
 
-      -- MIGRATED TO vim.lsp.config()
-      vim.lsp.config("pyright", {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          python = {
-            analysis = {
-              extraPaths = {
-                blender_lib_path,
-                blender_bl_operators_path,
-              },
-              autosearchPaths = true,
-              useLibraryCodeForTypes = true,
-            },
-          },
-        },
-      })
+			-- Capabilities for autocompletion
+			local capabilities = cmp_nvim_lsp.default_capabilities()
 
-      vim.lsp.config("lua_ls", {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            telemetry = { enable = false },
-          },
-        },
-      })
+			-- MIGRATED TO vim.lsp.config()
+			vim.lsp.config("pyright", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysis = {
+							extraPaths = {
+								blender_lib_path,
+								blender_bl_operators_path,
+							},
+							autosearchPaths = true,
+							useLibraryCodeForTypes = true,
+						},
+					},
+				},
+			})
 
-      vim.lsp.config("omnisharp", {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = { "OmniSharp" },
-        root_dir = function(fname)
-          local util = require("lspconfig.util")
-          return util.root_pattern("*.sln")(fname)
-              or util.root_pattern(".git")(fname)
-              or util.root_pattern("Assets")(fname)
-        end,
-        settings = {
-          omnisharp = {
-            useModernNet = true,
-            enableRoslynAnalyzers = true,
-            msbuild = { loadProjectsOnDemand = false },
-          },
-        },
-      })
+			vim.lsp.config("lua_ls", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+					},
+				},
+			})
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.prettier, -- JS, TS, JSON, etc.
-          null_ls.builtins.formatting.stylua, -- Lua formatting
-          null_ls.builtins.formatting.csharpier, -- C# Formatter
-          null_ls.builtins.formatting.rustywind, -- Rust Formatter
-          null_ls.builtins.diagnostics.esling_d,
-        },
-        on_attach = on_attach,
-      })
-    end,
-  },
+			vim.lsp.config("omnisharp", {
+				on_attach = on_attach,
+				capabilities = capabilities,
+				cmd = { "OmniSharp" },
+				root_dir = function(fname)
+					local util = require("lspconfig.util")
+					return util.root_pattern("*.sln")(fname)
+						or util.root_pattern(".git")(fname)
+						or util.root_pattern("Assets")(fname)
+				end,
+				settings = {
+					omnisharp = {
+						useModernNet = true,
+						enableRoslynAnalyzers = true,
+						msbuild = { loadProjectsOnDemand = false },
+					},
+				},
+			})
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.prettier, -- JS, TS, JSON, etc.
+					null_ls.builtins.formatting.stylua, -- Lua formatting
+					null_ls.builtins.formatting.csharpier, -- C# Formatter
+					null_ls.builtins.formatting.rustywind, -- Rust Formatter
+					null_ls.builtins.diagnostics.esling_d,
+				},
+				on_attach = on_attach,
+			})
+		end,
+	},
 
-  -- Mason (LSP Installer)
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    config = function()
-      require("mason").setup({
-        ui = {
-          border = "rounded",
-        },
-      })
-    end,
-  },
+	-- Mason (LSP Installer)
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup({
+				ui = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
 
-  -- Mason LSP Config (Ensures LSPs are installed)
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "pyright", "lua_ls", "omnisharp" },
-        automatic_installation = true,
-      })
-    end,
-  },
+	-- Mason LSP Config (Ensures LSPs are installed)
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "pyright", "lua_ls", "omnisharp" },
+				automatic_installation = true,
+			})
+		end,
+	},
 
-  -- Autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = {
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-e>"] = cmp.mapping.close(),
-        },
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = {
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-e>"] = cmp.mapping.close(),
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
+			})
+		end,
+	},
 }
